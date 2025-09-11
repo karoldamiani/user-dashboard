9efbda9116e4a0d17c8811fc502cfc8802381780
Kerolayne <karolgrassi.d@hotmail.com>
2025-09-11 17:35:52 -0300

    criando botão para mostrar mais detalhes de cada card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,52 +74,52 @@ export default function Home() {
 
   const handleSave = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    
+
     const formData = new FormData(event.currentTarget);
     const userData = {
-        firstName: formData.get("firstName"),
-        lastName: formData.get("lastName"),
-        email: formData.get("email"),
-        city: formData.get("city"),
-        age: formData.get("age"),
+      firstName: formData.get("firstName"),
+      lastName: formData.get("lastName"),
+      email: formData.get("email"),
+      city: formData.get("city"),
+      age: formData.get("age"),
     };
-    
+
     let url = "https://uoc2zyn2f1.execute-api.us-east-1.amazonaws.com/users";
     let method = "POST";
 
-    if (editingUser) {        
-        url = `${url}/${editingUser.id}`;
-        method = "PUT";
+    if (editingUser) {
+      url = `${url}/${editingUser.id}`;
+      method = "PUT";
     }
 
-    try {        
-        const res = await fetch(url, {
-            method: method,
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(userData),
-        });
+    try {
+      const res = await fetch(url, {
+        method: method,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(userData),
+      });
 
-        if (!res.ok) {
-            throw new Error(`Erro ao ${editingUser ? 'editar' : 'salvar'} o usuário.`);
-        }
+      if (!res.ok) {
+        throw new Error(`Erro ao ${editingUser ? 'editar' : 'salvar'} o usuário.`);
+      }
 
-        const savedUser = await res.json();
-        
-        if (editingUser) {           
-            setUsers(users.map(u => u.id === savedUser.id ? savedUser : u));
-        } else {            
-            setUsers((prev) => [...prev, savedUser]);
-        }       
-        
-        setModalIsOpen(false);
-        setEditingUser(null);
+      const savedUser = await res.json();
+
+      if (editingUser) {
+        setUsers(users.map(u => u.id === savedUser.id ? savedUser : u));
+      } else {
+        setUsers((prev) => [...prev, savedUser]);
+      }
+
+      setModalIsOpen(false);
+      setEditingUser(null);
 
     } catch (err) {
-        console.error("Erro na requisição:", err);
+      console.error("Erro na requisição:", err);
     }
-};
+  };
 
-  
+
   return (
     <main className="max-w-5xl mx-auto p-4">
       {/* search + new user */}
@@ -180,18 +180,25 @@ export default function Home() {
                 <div className="absolute right-0 top-10 z-20 w-24 bg-gray-800 border border-gray-700 rounded-lg shadow-lg">
                   <ul className="py-1">
                     <li>
-                      <button                       
-                        onClick={() => 
+                      <button
+                        onClick={() =>
                           handleEdit(user)}
-                        className="block w-full px-4 py-2 text-sm text-white hover:bg-gray-700"
+                        className="flex items-center w-full px-4 py-2 text-sm text-white hover:bg-gray-700"
                       >
+                        <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" className="mr-1.5 -ml-0.5 size-4 text-white">
+                          <path d="m2.695 14.762-1.262 3.155a.5.5 0 0 0 .65.65l3.155-1.262a4 4 0 0 0 1.343-.886L17.5 5.501a2.121 2.121 0 0 0-3-3L3.58 13.419a4 4 0 0 0-.885 1.343Z" />
+                        </svg>
                         Edit
                       </button>
                     </li>
                     <li>
-                      <button                      
-                        className="block w-full px-4 py-2 text-sm text-red-400 hover:bg-gray-700 hover:text-red-300"
+                      <button
+                        className="flex items-center w-full px-4 py-2 text-sm text-red-400 hover:bg-gray-700 hover:text-red-300"
                       >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                        </svg>
+
                         Delete
                       </button>
                     </li>
@@ -200,7 +207,7 @@ export default function Home() {
               )}
             </div>
             {/* card separado */}
-            <div className="flex flex-col items-center pb-10">
+            <div className="flex flex-col items-center pb-6 px-4 text-center">
               <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
                 {user.firstName} {user.lastName}
               </h5>
@@ -209,7 +216,15 @@ export default function Home() {
               </p>
               <p className="text-slate-600 leading-normal font-light">
                 📍 {user.city}
-              </p>
+              </p>             
+              <div className="w-full flex justify-center mt-8 mb-1">                
+                <button
+                  type="button"
+                  className="w-full sm:w-auto inline-flex items-center justify-center rounded-md bg-white/10 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-white/20"
+                >
+                  Show more
+                </button>
+              </div>
             </div>
           </div>
         ))}
@@ -237,64 +252,64 @@ export default function Home() {
               <div className="sm:col-span-2">
                 <label htmlFor="firstName" className="block text-sm/6 font-medium text-white">First name</label>
                 <div className="mt-2">
-                  <input id="firstName" 
-                  type="text" 
-                  name="firstName" 
-                  placeholder="First name" 
-                  autocomplete="given-name"
-                  defaultValue={editingUser ? editingUser.firstName : ''}
-                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                  <input id="firstName"
+                    type="text"
+                    name="firstName"
+                    placeholder="First name"
+                    autocomplete="given-name"
+                    defaultValue={editingUser ? editingUser.firstName : ''}
+                    className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
                 </div>
               </div>
 
               <div className="sm:col-span-2">
                 <label htmlFor="lastName" className="block text-sm/6 font-medium text-white">Last name</label>
                 <div className="mt-2">
-                  <input id="lastName" 
-                  type="text" 
-                  name="lastName" 
-                  placeholder="Last name" 
-                  autocomplete="family-name"
-                  defaultValue={editingUser ? editingUser.lastName : ''}
-                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                  <input id="lastName"
+                    type="text"
+                    name="lastName"
+                    placeholder="Last name"
+                    autocomplete="family-name"
+                    defaultValue={editingUser ? editingUser.lastName : ''}
+                    className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
                 </div>
               </div>
 
               <div className="sm:col-span-4">
                 <label htmlFor="email" className="block text-sm/6 font-medium text-white">Email address</label>
                 <div className="mt-2">
-                  <input id="email" 
-                  type="email" 
-                  name="email" 
-                  placeholder="Email" 
-                  autocomplete="email"
-                  defaultValue={editingUser ? editingUser.email : ''} 
-                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                  <input id="email"
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    autocomplete="email"
+                    defaultValue={editingUser ? editingUser.email : ''}
+                    className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
                 </div>
               </div>
 
               <div className="sm:col-span-2 sm:col-start-1">
                 <label htmlFor="city" className="block text-sm/6 font-medium text-white">City</label>
                 <div className="mt-2">
-                  <input id="city" 
-                  type="text" name="city" 
-                  placeholder="City" 
-                  autocomplete="address-level2"
-                  defaultValue={editingUser ? editingUser.city : ''}
-                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                  <input id="city"
+                    type="text" name="city"
+                    placeholder="City"
+                    autocomplete="address-level2"
+                    defaultValue={editingUser ? editingUser.city : ''}
+                    className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
                 </div>
               </div>
 
               <div className="sm:col-span-2">
                 <label htmlFor="region" className="block text-sm/6 font-medium text-white">Age</label>
                 <div className="mt-2">
-                  <input id="age" 
-                  type="number" 
-                  name="age" 
-                  placeholder="Age" 
-                  autocomplete="age"
-                  defaultValue={editingUser ? editingUser.age : ''} 
-                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                  <input id="age"
+                    type="number"
+                    name="age"
+                    placeholder="Age"
+                    autocomplete="age"
+                    defaultValue={editingUser ? editingUser.age : ''}
+                    className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
                 </div>
               </div>
 
